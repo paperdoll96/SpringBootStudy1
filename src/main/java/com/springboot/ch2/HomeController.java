1b907ebaedccae54698f313bb8e708a3d13ab788
82105 <ijh7476@naver.com>
2024-04-22 22:09:59 +0900

    수정

--- src/main/java/com/springboot/ch2/HomeController.java
+++ src/main/java/com/springboot/ch2/HomeController.java
@@ -5,6 +5,7 @@ package com.springboot.ch2;
 // 2. URL 과 메서드를 연결
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -15,5 +16,11 @@ public class HomeController { // 원격 프로그램
     public String main()
     {
         return "index"; // templates/index.html
+    }
+
+    @GetMapping("/")
+    public String test()
+    {
+        return "test"; // templates/index.html
     }
 }
